4fb3bc6baead127f4a0412670aadde508c1c907e
Tarunn06 <95083716+Tarunn06@users.noreply.github.com>
2024-08-06 10:27:23 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,88 @@
+import streamlit as st 
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import pipeline 
+import torch 
+import base64
+import os
+
+
+checkpoint = "t5-small" 
+offload_folder = "offload_weights"  
+
+
+if not os.path.exists(offload_folder):
+    os.makedirs(offload_folder)
+
+tokenizer = T5Tokenizer.from_pretrained(checkpoint)
+base_model = T5ForConditionalGeneration.from_pretrained(
+    checkpoint,
+    device_map='auto',
+    torch_dtype=torch.float32,
+    offload_folder=offload_folder
+)
+
+
+def file_preprocessing(file):
+    loader = PyPDFLoader(file)
+    pages = loader.load_and_split()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+    texts = text_splitter.split_documents(pages)
+    final_texts = ""
+    for text in texts:
+        print(text)
+        final_texts = final_texts + text.page_content
+    return final_texts 
+
+# LM pipeline 
+def llm_pipeline(filepath):
+    pipe_sum = pipeline(
+        'summarization',
+        model=base_model,
+        tokenizer=tokenizer,
+        max_length=500,
+        min_length=50
+    )
+    input_text = file_preprocessing(filepath)
+    result = pipe_sum(input_text)
+    result = result[0]['summary_text']
+    return result
+
+# Streamlit code
+@st.cache_data
+
+def displayPDF(file):
+    # Opening file from file path 
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+    # Embedding PDF in HTML 
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="600" type="application/pdf"></iframe>'
+
+    # Displaying a file 
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
+# Streamlit code
+st.set_page_config(layout='wide', page_title="Summarization App")
+
+def main():
+    st.title('MediSynapse-2')
+    uploaded_file = st.file_uploader("Upload your PDF File", type=['pdf'])
+
+    if uploaded_file is not None:
+        if st.button("Summarize"):
+            col1, col2 = st.columns(2)
+            filepath = "data/" + uploaded_file.name
+            with open(filepath, 'wb') as temp_file:
+                temp_file.write(uploaded_file.read())
+
+            with col1:
+                st.info("Uploaded PDF File")
+                pdf_viewer = displayPDF(filepath)
+            with col2:
+                st.info("Summarization is below")
+                summary = llm_pipeline(filepath)
+                st.success(summary)
+
+if __name__ == '__main__':
+    main()
